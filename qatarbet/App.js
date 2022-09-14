dd12921b21d5ba02d4cc9d1f2f4f628974610395
Ignacio Nicolás Fuentes <infuentes96@gmail.com>
2022-09-14 19:27:48 -0300

    implementando componente de Login

--- qatarbet/App.js
+++ qatarbet/App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+// login screen
+import LoginScreen from './src/screens/LoginScreen';
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <LoginScreen />
     </View>
   );
 }
