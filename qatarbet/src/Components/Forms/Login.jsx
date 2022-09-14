bbb4d0f45fd7263e079ca0f4b3b71f72c61dad7f
miguel linares <97300150+faramir07@users.noreply.github.com>
2022-09-14 16:05:26 -0500

    add componet landingpage and initi component login

---
+++ qatarbet/src/Components/Forms/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View } from "react-native";
+
+export const LandingPage = () => {
+
+  return (
+    <View>
+      soy el formulario de login
+    </View>
+  )
+}
+
