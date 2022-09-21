52e8d71cc98a02b1eacdb693b9d5aaf13c07dc6b
Maikyls <85658257+Maikyls@users.noreply.github.com>
2022-09-21 03:19:21 -0500

    se agregan fuentes y se estilan bets

--- qatarbet/src/components/bet-components/BetCard.jsx
+++ qatarbet/src/components/bet-components/BetCard.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 import { Text, View, StyleSheet, TouchableWithoutFeedback } from 'react-native'
 import BetModal from './BetModal';
 import { useState } from 'react';
+import { TouchableHighlight } from 'react-native-gesture-handler';
 
 
-export default function BetCard({bets}) {
+export default function BetCard({ bets }) {
 
   const [modalOpen, setModalOpen] = useState(false);
 
   const handleOnPress = (e) => {
     e.preventDefault();
     setModalOpen(true);
-  }
+  };
+
 
   return (
     <>
@@ -20,12 +22,16 @@ export default function BetCard({bets}) {
       >
         <View style={styles.card}>
           <View style={styles.spacing}>
-            <View style={styles.bgStyle}>
-              <Text style={styles.username}>{bets.user}</Text>
-              <Text>{bets.match}</Text>
-              <View style={styles.apuesta}>
-                <Text>{bets.money_bet}</Text>
-                <Text>{bets.final_profit === null || bets.final_profit === 0 ? 'Perdido' : 'Ganado'}</Text>
+            <View style={styles.shadow}>
+              <View style={styles.nameView}>
+                <Text style={styles.username}>{bets.user}</Text>
+              </View>
+              <View style={styles.matchView}>
+                <Text style={styles.match}>{bets.match}</Text>
+              </View>
+              <View style={styles.bet}>
+                <Text style={bets.final_profit === null || bets.final_profit === 0 ? styles.betAmountLost : styles.betAmountWon}>${bets.money_bet}</Text>
+                <Text style={bets.final_profit === null || bets.final_profit === 0 ? styles.betResultLost : styles.betResultWon}>{bets.final_profit === null || bets.final_profit === 0 ? 'Perdido' : 'Ganado'}</Text>
               </View>
             </View>
           </View>
@@ -35,7 +41,7 @@ export default function BetCard({bets}) {
         modalOpen={modalOpen}
         setModalOpen={setModalOpen}
       />
-    </>   
+    </>
   )
 }
 
@@ -43,31 +49,95 @@ const styles = StyleSheet.create({
   card: {
     flex: 1,
     height: 130,
-    marginBottom: 5
+    marginBottom: 15
   },
   spacing: {
+    // backgroundColor: 'yellow',
     flex: 1,
-    padding: 5,
+    paddingHorizontal: 5,
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
   },
-  bgStyle: {
-    backgroundColor: '#FFFFFF',
-    height: '100%',
-    borderRadius: 10,
-    alignItems: 'center',
-    justifyContent: 'space-around'
+  // shadow: {
+  //   shadowColor: "#000",
+  //   shadowOffset: {
+  //     width: 0,
+  //     height: 2,
+  //   },
+  //   shadowOpacity: 0.25,
+  //   shadowRadius: 3.84,
+
+  //   elevation: 5,
+  // },
+  nameView: {
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    backgroundColor: '#D20A46',
+    borderBottomWidth: 2,
+    borderBottomColor: '#E4E4E4',
+    height: 40,
   },
   username: {
-    width:'100%',
-    textAlign:'center',
+    // height: 40,
+    width: '100%',
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    fontWeight: '700',
+    color: '#F8F8FA',
+    fontFamily: 'Roboto',
     paddingVertical: 5,
+    marginVertical: 5,
     height: 30,
-    borderBottomWidth: 1,
-    borderBottomColor: '#F8F8FA',
-    backgroundColor:'yellow'
   },
-  apuesta: {
+  matchView: {
+    borderBottomWidth: 2,
+    borderBottomColor: '#E4E4E4',
+    height: 45,
+    borderLeftWidth: 2,
+    borderLeftColor: '#E4E4E4',
+    borderRightWidth: 2,
+    borderRightColor: '#E4E4E4',
+  },
+  match: {
+    width: '100%',
+    textAlign: 'center',
+    fontFamily: 'Koulen',
+    fontSize: 17,
+    paddingTop: 6,
+  },
+  bet: {
+    // backgroundColor: 'yellow',
+    height: 40,
     width: '100%',
     flexDirection: 'row',
-    justifyContent: 'space-around'
-  }
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    borderBottomWidth: 2,
+    borderBottomColor: '#E4E4E4',
+    borderLeftWidth: 2,
+    borderLeftColor: '#E4E4E4',
+    borderRightWidth: 2,
+    borderRightColor: '#E4E4E4',
+  },
+  betAmountLost: {
+    fontWeight: '500',
+    fontSize: 17,
+    color: '#D20A46'
+  },
+  betAmountWon: {
+    fontWeight: '500',
+    fontSize: 17,
+    color: 'green'
+  },
+  betResultLost: {
+    fontWeight: '500',
+    fontSize: 17,
+    color: '#D20A46'
+  },
+  betResultWon: {
+    fontWeight: '500',
+    fontSize: 17,
+    color: 'green'
+  },
+
 })
